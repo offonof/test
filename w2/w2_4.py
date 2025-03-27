39d41ff28e29270b56d5d5bb2806ea62bf338e0f
offonof <117250178+offonof@users.noreply.github.com>
2025-03-27 13:00:42 +0900

    Update w2_4.py

--- w2/w2_4.py
+++ w2/w2_4.py
@@ -11,17 +11,15 @@
 상덕이가 종이에 적어놓은 내용과 바퀴의 칸의 수가 주어졌을 때, 바퀴에 적어놓은 알파벳을 알아내는 프로그램을 작성하시오.
 """
 
-import sys
-
 # 입력 받기
-N, K = map(int, sys.stdin.readline().split())
+N, K = map(int, input().split())
 wheel = ['?'] * N  # 바퀴 초기화
 used = set()  # 이미 사용된 알파벳 저장
 
 position = 0  # 화살표가 가리키는 위치
 
 for _ in range(K):
-    S, C = sys.stdin.readline().split()
+    S, C = input().split()
     S = int(S)  # 시계 방향으로 회전하는 횟수
 
     position = (position - S) % N  # 시계 방향 회전
@@ -30,12 +28,12 @@ for _ in range(K):
     if wheel[position] == '?':  # 빈 자리라면 알파벳 채우기
         if C in used:  # 이미 사용된 알파벳이라면 불가능한 경우
             print("!")
-            sys.exit()
+            exit()  # 프로그램 종료
         wheel[position] = C
         used.add(C)
     elif wheel[position] != C:  # 다른 알파벳이 이미 있으면 불가능
         print("!")
-        sys.exit()
+        exit()  # 프로그램 종료
 
 # 바퀴를 현재 화살표 위치에서 역순으로 출력
 result = ''.join(wheel[position:] + wheel[:position])
